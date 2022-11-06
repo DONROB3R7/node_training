e6aabb278be30407942bb1afe252ac332db9c850
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-11-06 12:06:52 +0100

    Server with node

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    res.write('<head><link rel="styleseet" href="#"/></head>')
-    res.write('<p>Hello, ninja</p>');
-    res.write('<p>Hello ninja again</p>');
-    res.end();
+   // set header content type 
+   res.setHeader('Content-Type', 'text/html');
+
+   let path = './views/';
+
+   switch(req.url){
+    case '/': 
+        path += 'index.html';
+        res.statusCode = 200;
+        break;
+    case '/about':
+        path += 'about.html';
+        res.statusCode = 200;
+        break
+    case '/about-me':
+        res.setHeader('Location' , '/about')
+        res.statusCode = 301;
+        res.end();
+        break
+    default: 
+        path += '404.html';
+        res.statusCode = 404;
+        break;
+   }
+
+   // send and html file 
+   fs.readFile(path, (err, data) =>{
+    if(err){
+        console.log(err);
+    } else {
+        //res.write(data);  
+        res.end(data);
+    }
+   });
+
 });
 
 server.listen(3000, 'localhost', () => {
